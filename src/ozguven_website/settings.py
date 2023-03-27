d8440d057d5498b75ff6584d0e90a2adff5bbdca
Ahmet Hakan Özgüven <99harkan@gmail.com>
2023-03-27 13:13:53 +0000

    linux deploy version

--- src/ozguven_website/settings.py
+++ src/ozguven_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = ['www.ahmethakanozguven.me','ahmethakanozguven.me','207.154.238.137']
 
 
 
